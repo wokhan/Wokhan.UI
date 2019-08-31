7bf4215940c1ba97d477e72229745ced4c062848
Khan <wokhan@outlook.com>
2019-08-31 21:02:14 +0200

    Updated MSBuild.Sdk.Extras, minor modifications

--- Wokhan.UWP/UI/Xaml/Controls/GridViewGrid.cs
+++ Wokhan.UWP/UI/Xaml/Controls/GridViewGrid.cs
@@ -1,5 +1,4 @@
-﻿using Windows.Foundation;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page https://go.microsoft.com/fwlink/?LinkId=234236
