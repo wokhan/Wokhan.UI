1ba6b9c42483652c6083bdd7b26fac52972b0aec
Khan <wokhan@outlook.com>
2021-02-25 14:08:17 +0100

    Removed DecoratedTabItem (replaced by CustomAdorner).

--- Wokhan.UI/Xaml/Extensibility/CustomPropertyExtender.cs
+++ Wokhan.UI/Xaml/Extensibility/CustomPropertyExtender.cs
@@ -1,5 +1,10 @@
 ﻿using System.Windows;
+#if __WPF__
 using System.Windows.Controls;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#endif
 
 namespace Wokhan.UI.Xaml.Extensibility
 {
@@ -8,13 +13,13 @@ namespace Wokhan.UI.Xaml.Extensibility
         public class CustomProperty
         {
             public string Title { get; set; }
-            public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(CustomProperty));
+            public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(CustomProperty), null);
             
             public object Value { get; set; }
-            public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(object), typeof(CustomProperty));
+            public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(object), typeof(CustomProperty), null);
         }
 
-        public static readonly DependencyProperty CustomPropertiesProperty = DependencyProperty.RegisterAttached("CustomProperties", typeof(CustomProperty), typeof(ContentControl));
+        public static readonly DependencyProperty CustomPropertiesProperty = DependencyProperty.RegisterAttached("CustomProperties", typeof(CustomProperty), typeof(ContentControl), null);
         public static void SetCustomProperties(Control control, CustomProperty value) => control.SetValue(CustomPropertiesProperty, value);
         public static CustomProperty GetCustomProperties(Control control) => (CustomProperty)control.GetValue(CustomPropertiesProperty);
     }
